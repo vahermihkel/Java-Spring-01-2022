543547e3901e228122d8ac4002e3c25a9ca50eee
mihkelvaher <mihkel.vaher@industry62.com>
2022-04-03 13:06:13 +0300

    unit testid

---
+++ webshop/src/test/java/ee/mihkel/webshop/TestProduct.java
@@ -0,0 +1,12 @@
+package ee.mihkel.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestProduct {
+
+    @Test
+    void contextLoads() {
+    }
+}
--- webshop/src/test/java/ee/mihkel/webshop/WebshopApplicationTests.java
+++ webshop/src/test/java/ee/mihkel/webshop/WebshopApplicationTests.java
@@ -1,13 +1,44 @@
 package ee.mihkel.webshop;
 
+import ee.mihkel.webshop.model.entity.Category;
+import ee.mihkel.webshop.model.entity.Product;
+import ee.mihkel.webshop.repository.CategoryRepository;
+import ee.mihkel.webshop.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WebshopApplicationTests {
 
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void assertThatAddedProductExistsInRepository_whenAdded() {
+        Category category = categoryRepository.findById(1L).get();
+        Product testProduct = new Product(
+                99L,
+                "Toode2",
+                1.0,
+                1, "pildi_url",
+                true,
+                category,
+                "desc",
+                100);
+        productRepository.save(testProduct);
+        Product product = productRepository.findById(99L).get();
+        Assertions.assertEquals(
+                "Toode2",
+                product.getName());
     }
 
 }
